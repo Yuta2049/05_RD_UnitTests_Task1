acbbc97c365026b9e3e6707d970b4b16becb39a0
Vinci2049 <vinci2049@gmail.com>
2018-11-29 09:42:47 +0400

    1.0

--- src/main/java/Utils.java
+++ src/main/java/Utils.java
@@ -2,17 +2,23 @@ public class Utils {
 
     public static String concantenateWords(String word1, String word2) {
 
-        String word3 = word1 + word2;
+        if (word1 == null || word2 == null) {
+            throw new NullPointerException("Null вместо строки");
+        }
 
-        return word3;
+        return word1 + word2;
     }
 
 
     public static int computeFactorial(int Number) {
 
+        if (Number < 0) {
+            throw new IllegalArgumentException("Отрицательное число для вычисления факториала");
+        }
+
         int factorial = 1;
-        for (int i=1; i<=Number; i++) {
-            factorial = factorial*i;
+        for (int i = 1; i <= Number; i++) {
+            factorial = factorial * i;
         }
 
         return factorial;
--- src/test/java/UnitTests.java
+++ src/test/java/UnitTests.java
@@ -1,57 +1,81 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTimeout;
-
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class UnitTests {
 
 
     @Test
     void concantenateWords() {
-        //assertEquals(2, 1 + 1);
 
         assertEquals("onetwo", Utils.concantenateWords("one", "two"));
-        assertEquals("one", Utils.concantenateWords("one", null));
-        assertEquals("one", Utils.concantenateWords("one", ""));
-        assertEquals("oneБУ", Utils.concantenateWords("one", "БУ"));
+        assertEquals("onethree", Utils.concantenateWords("one", "three"));
 
     }
 
     @Test
-    void testComputeFactorial() {
+    void concantenateWordsCorrectlyWorksWithNonLatinSymbols() {
 
-        assertEquals(6, Utils.computeFactorial(3), "Факториал 3");
-        assertEquals(24, Utils.computeFactorial(4), "Факториал 4");
-        //assertEquals(2, 1 + 4);
-
-        assertEquals("onetwo", Utils.concantenateWords("one", "two"));
+        assertEquals("ОдинДва", Utils.concantenateWords("Один", "Два"));
+        assertEquals("oneдва", Utils.concantenateWords("one", "два"));
 
     }
 
+    @Test
+    void concantenateWordsCorrectlyWorksWithEmptyString() {
+
+        assertEquals("one", Utils.concantenateWords("one", ""));
+        assertEquals("", Utils.concantenateWords("", ""));
+
+    }
+
+    @Test
+    void concantenateWordsThrowsNPEIfStringIsNull() {
+
+        assertThrows(NullPointerException.class, () -> Utils.concantenateWords("one", null));
+        assertThrows(NullPointerException.class, () -> Utils.concantenateWords(null, "two"));
+        assertThrows(NullPointerException.class, () -> Utils.concantenateWords(null, null));
+
+    }
+
+    @Test
+    //@Disabled
+    void testComputeFactorial() {
+
+        assertEquals(1, Utils.computeFactorial(0), "Факториал 0");
+        assertEquals(6, Utils.computeFactorial(3), "Факториал 3");
+        assertEquals(24, Utils.computeFactorial(4), "Факториал 4");
+
+    }
+
+    @Test
+     void testComputeFactorialThrowsIllegalArgumentExceptionWhenGetsNegativeNumber() {
+
+        assertThrows(IllegalArgumentException.class, () -> Utils.computeFactorial(-3));
+        assertThrows(IllegalArgumentException.class, () -> Utils.computeFactorial(-5));
+
+    }
 
     @Test
     void testFactorialWithTimeout() {
 
         Random random = new Random();
-        int number = 0;
+        int number;
         do {
             number = random.nextInt();
         } while (number < 0);
 
-        System.out.println(number);
-
         int ar[] = {number};
 
         assertTimeout(Duration.ofMillis(1000), () -> {
-            Utils.computeFactorial(ar[0]);
-        });
-
-
+                    Utils.computeFactorial(ar[0]);
+                },
+                "Факториал числа: " + ar[0]);
 
     }
-
 
 }
